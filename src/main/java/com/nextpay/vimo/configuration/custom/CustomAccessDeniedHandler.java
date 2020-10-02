c659d30fe3163086817df476ffb1f5af55d83958
HuynhDuc1701 <36859336+HuynhDuc1701@users.noreply.github.com>
2020-10-02 08:43:37 +0700

    Update CustomAccessDeniedHandler.java

--- src/main/java/com/nextpay/vimo/configuration/custom/CustomAccessDeniedHandler.java
+++ src/main/java/com/nextpay/vimo/configuration/custom/CustomAccessDeniedHandler.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+//change on remote
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
